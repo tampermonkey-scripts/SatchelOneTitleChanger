90fac875f5c2d928736a60244ddc961ff897806f
devmoksh <moksh.choudry@gmail.com>
2021-04-08 16:32:30 +0100

    Initial Commit

---
+++ userscript.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         SatchelOne Title Changer
+// @namespace    https://www
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://satchelone.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  function runProgram() {}
+
+  setTimeout(runProgram, 6000);
+})();
